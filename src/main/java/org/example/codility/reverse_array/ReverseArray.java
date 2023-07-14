a20d368a76ebac0d5a890d435dc9fd2e28688b42
tramhuuducvn <tramhuuduc@gmail.com>
2023-07-14 23:15:24 +0700

    add reversing array

---
+++ src/main/java/org/example/codility/reverse_array/ReverseArray.java
@@ -0,0 +1,15 @@
+package org.example.codility.reverse_array;
+
+public class ReverseArray {
+    public static int[] solution(int[] A) {
+        int size = A.length;
+
+        for (int i = 0; i < size / 2; ++i) {
+            int temp = A[i];
+            A[i] = A[size - i - 1];
+            A[size - i - 1] = temp;
+        }
+
+        return A;
+    }
+}
---
+++ src/test/java/org/example/codility/reverse_array/ReverseArrayTest.java
@@ -0,0 +1,46 @@
+package org.example.codility.reverse_array;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ReverseArrayTest {
+    @Test
+    void test_1() {
+        int[] A = { 1, 2, 3, 4, 5 };
+        int[] E = { 5, 4, 3, 2, 1 };
+
+        Assertions.assertArrayEquals(E, ReverseArray.solution(A));
+    }
+
+    @Test
+    void test_2() {
+        int[] A = { 7, 5, 4, 3, 2, 1 };
+        int[] E = { 1, 2, 3, 4, 5, 7 };
+
+        Assertions.assertArrayEquals(E, ReverseArray.solution(A));
+    }
+
+    @Test
+    void test_3() {
+        int[] A = { -1, -3 };
+        int[] E = { -3, -1 };
+
+        Assertions.assertArrayEquals(E, ReverseArray.solution(A));
+    }
+
+    @Test
+    void test_4() {
+        int[] A = {};
+        int[] E = {};
+
+        Assertions.assertArrayEquals(E, ReverseArray.solution(A));
+    }
+
+    @Test
+    void test_5() {
+        int[] A = { 1 };
+        int[] E = { 1 };
+
+        Assertions.assertArrayEquals(E, ReverseArray.solution(A));
+    }
+}
