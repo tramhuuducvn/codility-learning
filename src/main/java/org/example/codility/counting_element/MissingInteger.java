545e21a2e7782f44e3836e6d6c0d834edc248bf0
tramhuuducvn <tramhuuduc@gmail.com>
2023-11-12 21:43:53 +0700

    add comment to codility folder

--- src/main/java/org/example/codility/counting_element/MissingInteger.java
+++ src/main/java/org/example/codility/counting_element/MissingInteger.java
@@ -1,4 +1,35 @@
+// https://app.codility.com/programmers/lessons/4-counting_elements/missing_integer/
+
+/**
+ * 
+This is a demo task.
+
+Write a function:
+
+class Solution { public int solution(int[] A); }
+
+that, given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
+
+For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
+
+Given A = [1, 2, 3], the function should return 4.
+
+Given A = [−1, −3], the function should return 1.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [1..100,000];
+each element of array A is an integer within the range [−1,000,000..1,000,000].
+
+*/
+
 package org.example.codility.counting_element;
+
+/*
+ * Main Idea:
+ * Iterate through the boolean array and use that array to mark the presence of positive integers.
+ * After marking, find the first unmarked index, which represents the smallest missing positive integer.
+ */
 
 public class MissingInteger {
     public static int solution(int[] A) {
--- src/main/java/org/example/codility/interations/BinaryGap.java
+++ src/main/java/org/example/codility/interations/BinaryGap.java
@@ -1,6 +1,35 @@
 //Reference: https://app.codility.com/programmers/lessons/1-iterations/binary_gap/
 package org.example.codility.interations;
 
+/**
+ * A binary gap within a positive integer N is any maximal sequence of
+ * consecutive zeros that is surrounded by ones at both ends in the binary
+ * representation of N.
+ * 
+ * For example, number 9 has binary representation 1001 and contains a binary
+ * gap of length 2. The number 529 has binary representation 1000010001 and
+ * contains two binary gaps: one of length 4 and one of length 3. The number 20
+ * has binary representation 10100 and contains one binary gap of length 1. The
+ * number 15 has binary representation 1111 and has no binary gaps. The number
+ * 32 has binary representation 100000 and has no binary gaps.
+ * 
+ * Write a function:
+ * 
+ * class Solution { public int solution(int N); }
+ * 
+ * that, given a positive integer N, returns the length of its longest binary
+ * gap. The function should return 0 if N doesn't contain a binary gap.
+ * 
+ * For example, given N = 1041 the function should return 5, because N has
+ * binary representation 10000010001 and so its longest binary gap is of length
+ * 5. Given N = 32 the function should return 0, because N has binary
+ * representation '100000' and thus no binary gaps.
+ * 
+ * Write an efficient algorithm for the following assumptions:
+ * 
+ * N is an integer within the range [1..2,147,483,647].
+ */
+
 public class BinaryGap {
     public static String convertToBinary(int N) {
         StringBuilder str = new StringBuilder("");
@@ -21,6 +50,7 @@ public class BinaryGap {
             index++;
         }
 
+        // init max value
         int max = 0;
 
         for (; index < size; ++index) {
@@ -32,13 +62,18 @@ public class BinaryGap {
                 index++;
             }
 
+            // in the case index >= size that mean the end of array is not "1" => the
+            // current
+            // zeros sequence is not surrounded by ones => count = 0
             if (index >= size) {
                 count = 0;
             }
-
+            // find the max
             if (count > max) {
                 max = count;
             }
+            // This line is very important because it make sure that we don't miss any
+            // element in the complex nested loop
             index--;
         }
 
