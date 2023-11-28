b2450d65939e37c1b12f4278f5a69e5eab65b023
ductram <ductram@kms-technology.com>
2023-11-28 19:44:41 +0700

    save changes

---
+++ src/main/java/org/example/codility/counting_element/MaxCounters.java
@@ -0,0 +1,39 @@
+package org.example.codility.counting_element;
+
+public class MaxCounters {
+
+    public static int increase(int[] src, int index) {
+        if (index > -1 && index < src.length) {
+            src[index] += 1;
+            return src[index];
+        }
+        return 0;
+    }
+
+    public static void setToValue(int[] src, int value) {
+        for (int i = 0; i < src.length; ++i) {
+            src[i] = value;
+        }
+    }
+
+    public static int[] solution(int N, int[] A) {
+        int[] result = new int[N];
+        int max = 0;
+
+        for (int a : A) {
+
+            if (a == N + 1) {
+                setToValue(result, max);
+                continue;
+            }
+
+            int temp = increase(result, a - 1);
+
+            if (temp > max) {
+                max = temp;
+            }
+        }
+
+        return result;
+    }
+}
