2012cee71dcbeaa5045cb7a4751189f9aba4f5a8
ductram <ductram@kms-technology.com>
2023-11-30 09:21:13 +0700

    save

--- src/test/java/org/example/codility/counting_element/MaxCountersTest.java
+++ src/test/java/org/example/codility/counting_element/MaxCountersTest.java
@@ -1,8 +1,12 @@
 package org.example.codility.counting_element;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.platform.commons.util.StringUtils;
 
 public class MaxCountersTest {
     // example test
@@ -64,6 +68,36 @@ public class MaxCountersTest {
         int[] expected = { 2, 2, 2, 2 };
 
         assertArrayEquals(expected, actual);
+    }
+
+    /**
+     * (0, 0, 0, 0)
+     * (1, 0, 0, 0)
+     * (1, 0, 1, 0)
+     * (1, 1, 1, 1)
+     * (1, 1, 1, 1)
+     * (1, 1, 1, 2)
+     * (2, 2, 2, 2)
+     * (2, 3, 2, 2)
+     * (2, 4, 2, 2)
+     * (4, 4, 4, 4)
+     */
+    @Test
+    void correctness_3() {
+        // Given info
+        int N = 4;
+        int[] A = { 1, 3, 5, 5, 4, 5, 2, 2, 5 };
+        // Check
+        int[] actual = MaxCounters.solution(N, A);
+        int[] expected = { 4, 4, 4, 4 };
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "racecar", "radar", "able was I ere I saw elba" })
+    void palindromes(String candidate) {
+        assertTrue(StringUtils.isNotBlank(candidate));
     }
 
 }
