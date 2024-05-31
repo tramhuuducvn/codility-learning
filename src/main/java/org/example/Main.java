26f28e6085d8ff56eac1423aca1fda7d3d21a31c
tramhuuducvn <tramhuuduc@gmail.com>
2024-05-31 21:28:07 +0700

    test-commit-1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,30 +1,8 @@
 package org.example;
 
-import java.util.Arrays;
-import java.util.List;
-
-import org.apache.commons.collections4.ListUtils;
-import org.example.util.Logger;
-
 public class Main {
 
     public static void main(String[] args) {
-        // int n = 10;
-        // int k = 4;
-        // double r = n / (double) k;
-        // Logger.log(r);
-
-        // int[] nums = { 1, 2, 3, 4 };
-        // Logger.log(nums.length);
-
-        int a = 7;
-        switch (a) {
-            case 9:
-                Logger.log(a);
-                break;
-
-            default:
-                break;
-        }
+        // Hello
     }
 }
