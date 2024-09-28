2744504e6a111b2057251994e9a4e1cdb808053f
tramhuuducvn <tramhuuduc@gmail.com>
2024-09-28 23:39:28 +0700

    Implement Middleware.java

--- src/main/java/org/example/design_pattern/behavioral/chain_of_responsibility/demo_1/middleware/Middleware.java
+++ src/main/java/org/example/design_pattern/behavioral/chain_of_responsibility/demo_1/middleware/Middleware.java
@@ -1,5 +1,49 @@
 package org.example.design_pattern.behavioral.chain_of_responsibility.demo_1.middleware;
 
+/**
+ * Base middleware class
+ */
 public abstract class Middleware {
+    private Middleware next;
 
+    /**
+     * Build chain of middleware objects
+     * 
+     * @param first
+     * @param chain
+     * @return middleware
+     */
+    public static Middleware link(Middleware first, Middleware... chain) {
+        Middleware head = first;
+        for (Middleware nextInChain : chain) {
+            head.next = nextInChain;
+            head = nextInChain;
+        }
+        return first;
+    }
+
+    /**
+     * Subclasses will implement this method with concrete checks
+     * 
+     * @param email
+     * @param password
+     * @return
+     */
+    public abstract boolean check(String email, String password);
+
+    /**
+     * Runs checks in the next object in chain or ends traversing if we're in the
+     * last object in chain.
+     * 
+     * @param email
+     * @param password
+     * @return
+     */
+    protected boolean checkNext(String email, String password) {
+        if (next == null) {
+            return true;
+        }
+
+        return next.check(email, password);
+    }
 }
