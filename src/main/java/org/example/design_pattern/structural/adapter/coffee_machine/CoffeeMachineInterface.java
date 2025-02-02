b73ae5bc1a41c7527e5a3a95381f3a6941eb006f
tramhuuducvn <tramhuuduc.vn@gmail.com>
2025-02-02 08:19:33 +0700

    adapter-design-pattern

---
+++ src/main/java/org/example/design_pattern/structural/adapter/coffee_machine/CoffeeMachineInterface.java
@@ -0,0 +1,8 @@
+package org.example.design_pattern.structural.adapter.coffee_machine;
+
+public interface CoffeeMachineInterface {
+
+    void chooseFirstSelection();
+
+    void chooseSecondSelection();
+}
---
+++ src/main/java/org/example/design_pattern/structural/adapter/coffee_machine/OldCoffeeMachine.java
@@ -0,0 +1,12 @@
+package org.example.design_pattern.structural.adapter.coffee_machine;
+
+public class OldCoffeeMachine {
+
+    public void selectA() {
+        System.out.println("Selected A");
+    }
+
+    public void selectB() {
+        System.out.println("Selected B");
+    }
+}
